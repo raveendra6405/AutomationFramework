506b8f9bd4bb018b4b61152b0103605a2d75838a
raveendra6405 <raveendramadhali@gmail.com>
2021-03-16 10:57:44 +0530

    java

---
+++ SeleniumActitime/src/com/Actitime/project/ProgramDemo.java
@@ -0,0 +1,18 @@
+package com.Actitime.project;
+class A
+{
+	A()
+	{
+      System.out.println();
+    }
+}
+public class ProgramDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		new A();
+	}
+	
+
+}
